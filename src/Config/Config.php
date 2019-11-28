8f9c31b786ead97ae3579701e88abf24d12b8b07
sigma-z <sigma_z@sigma-scripts.de>
2019-11-28 22:35:40 +0100

    Add Config and added my full lastname to license

---
+++ src/Config/Config.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Simtt\Config;
+
+/**
+ * @author Steffen Zeidler <sigma_z@sigma-scripts.de>
+ */
+class Config
+{
+
+    /** @var string */
+    private $logDir;
+
+    /** @var int */
+    private $precision = 1;
+
+    /** @var int */
+    private $showLogItems = 15;
+
+    /** @var int */
+    private $showTaskItems = 15;
+
+
+    public function getLogDir(): string
+    {
+        return $this->logDir;
+    }
+
+    public function getPrecision(): int
+    {
+        return $this->precision;
+    }
+
+    public function getShowLogItems(): int
+    {
+        return $this->showLogItems;
+    }
+
+    public function getShowTaskItems(): int
+    {
+        return $this->showTaskItems;
+    }
+}
