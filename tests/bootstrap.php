fc43054e23f99adefcbf0ceb4854e5d2870036dc
sigma-z <sigma_z@sigma-scripts.de>
2019-12-25 21:36:36 +0100

    Update composer dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,7 @@ declare(strict_types=1);
  */
 require_once __DIR__ . '/../src/bootstrap.php';
 require_once __DIR__ . '/Helper/DIContainer.php';
+require_once __DIR__ . '/Helper/LogEntryCreator.php';
+require_once __DIR__ . '/Helper/VirtualFileSystem.php';
 
 \Helper\DIContainer::$container = $containerBuilder;
