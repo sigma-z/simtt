bbca69470851178cffd438caa20055378b0ee40d
sigma-z <sigma_z@sigma-scripts.de>
2020-02-10 22:42:32 +0100

    Debug github ci-action

--- src/Application/Command/TimerCommand.php
+++ src/Application/Command/TimerCommand.php
@@ -117,7 +117,7 @@ abstract class TimerCommand extends Command
 
     private function shouldPromptForComment(InputInterface $input): bool
     {
-        var_dump($input->isInteractive());
+        var_dump(getenv('SHELL_INTERACTIVE'));
         return $input->isInteractive();
     }
 
